de25028c9bbb561312d3a99bf0d38b637601d90f
Marco Stixenberger <marco.stixenberger@gmail.com>
2020-03-10 20:47:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,77 +11,62 @@ from functions_and_modules import *
 ##########################################################   SETUP  #################################################################################
 #setup is executed once at startup
 #setting permissions (necessary for sending image)
-print('setting permissions...')
-os.system('sudo chmod -R 777 .')
+#print('setting permissions...')
+#os.system('sudo chmod -R 777 .')
 
 #pin declaration:
 #lamp and pump are connected to the double relais module
 #fan1: humidity regulation, fan2: inhouse ventilation (air movement)
 lamp_pin = 17
 filter_pin = 27
-fan1_pin = 21
-fan2_pin = 20
-dht1_pin = 4
+led_fan_pin = 21
 buzzer_pin = 22
 
 
 lamp = io.LED(pin=lamp_pin, active_high=False)
 filter = io.LED(pin=filter_pin, active_high=False)
 buzzer = io.TonalBuzzer(buzzer_pin)
-fan1 = io.PWMLED(fan1_pin)
-fan2 = io.PWMLED(fan2_pin)
+led_fan = io.PWMLED(led_fan_pin)
+
 bot = telegram.Bot(token=token) #token comes from your configuration.py file
 
 
 #variable initialisation:
 errorcounter = 0
-temperature = 0
+water_temp = 0
+led_temp = 0
 hours = 0
 hours_old = 0
 minutes = 0
 minutes_old = 0
 cycles = 0                 #cyclenumber for debugging
-wateringcycles = 0
 emergencystate = False
 lampstate = False
-humidity_list = []
 temperature_list = []
 timestamp_list = []
 seconds_since_start_list = []
-co2_list = []
-tvoc_list = []
+
 
 #parameter declaration:
-watering_active = False
 maxerrors = 5
-daytime_interval = (10,18)  #time interval for lights on
-pumptime = 8              #seconds for plantwatering per wateringcycle
+lighttime_interval = (10,18)  #time interval for lights on
 main_delay = 1             #delay in seconds for main loop
 #chat_id = set your telegram chat id here (or from configuration file)
 
 #absolute maximum values:
-
+# placeholder
 
 #set device states (setup)
 lamp.off()
 filter.off()
-fan1.off()
+led_fan.off()
 
 beep(buzzer)      #initial startup beep
 
 start_time = round(set_starttime(), 1)
 
 print('starting up...\n')
-bot.send_message(chat_id=chat_id, text='Hello Sir, i am starting up now...\n')
-
-print('venting air on startup...')
-bot.send_message(chat_id=chat_id, text='venting air on startup...\n')
-fan1.on()
-fan2.on()
-sleep(15)
-fan1.off()
-fan2.off()
-
+#bot.send_message(chat_id=chat_id, text='Hello Sir, i am starting up now...\n')
 hours = gethours()
 minutes = getminutes()
 oldhours = hours
@@ -95,43 +80,26 @@ while(True):
         minutes = getminutes()
         timestamp = gettimestamp()
 
-        #shutdown in the evening :
-        if(hours==21):
-            print('good night! - shutting down...')
-            bot.send_message(chat_id, text='good night! - shutting down...')
-            os.system("sudo shutdown now")
 
-
-        #reboot in the morning :
-        if((hours==9 and hours!=oldhours)):
-            print('good morning! - rebooting...')
-            bot.send_message(chat_id, text='good morning! - rebooting...')
-            os.system("sudo shutdown -r now")
-
-
-        #check if daytime:
-        if(hours>=daytime_interval[0] and hours<daytime_interval[1]):
-            daytime = True
+        #check if lighttime:
+        if(hours>=lighttime_interval[0] and hours<lighttime_interval[1]):
+            lighttime = True
         else:
-            daytime = False
+            lighttime = False
 
 
         #light control:
-        if(daytime==True and emergencystate==False and humidity>humidity_min and humidity<85 and temperature<=temp_max 
-        and temperature>=temp_min):
+        if(lighttime==True and emergencystate==False):
             lamp.on()
             lampstate = True
         else:
             lamp.off()
             lampstate = False
 
-            
-
-
-            
 
         #printing out information
-        print('Temperature: {}'.format(temperature) + ' deg')
+        print('Water Temperature: {}'.format(water_temp) + ' deg')
+        print('Led Temperature: {}'.format(led_temp) + ' deg')
         print('Cycles: {}'.format(cycles))
         print('Seconds since program start: {}'.format(int(round(time_since_start(start_time), 0))))
         if lampstate==True:
