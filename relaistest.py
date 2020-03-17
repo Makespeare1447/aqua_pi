b6e683bd6f652a37e6cfd7f106a8f2108309632a
Marco Stixenberger <marco.stixenberger@gmail.com>
2020-03-17 14:57:11 +0100

    Update relaistest.py

--- relaistest.py
+++ relaistest.py
@@ -1,15 +1,15 @@
 import gpiozero as io
 from time import sleep
 
-relais_1 = io.LED(pin=20, active_high=False)
-#relais_2 = io.LED(pin=21, active_high=False)
+relais_1 = io.LED(pin=20, active_high=False)    #filter
+relais_2 = io.LED(pin=21, active_high=False)    #lamp
 
 sleep(1)
 relais_1.on()
-#relais_2.on()
-sleep(10)
+relais_2.on()
+sleep(3)
 relais_1.off()
-#relais_2.off()
+relais_2.off()
 
 
 print('Success! - you reached the end of the Program!')
